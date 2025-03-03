2b6efac93eaaba0675cddb71933db0452d58bfe4
John Noel <jnoelbentinganan@gmail.com>
2025-03-03 22:06:04 +1100

    work in progress...

---
+++ app.py
@@ -0,0 +1,12 @@
+print(35 * "*")
+print("** \t \t \t \t **")
+print("** \t Welcome to PyBank \t **")
+print("** \t \t \t \t **")
+print(35 * "*")
+print("\n")
+
+# Funds
+funds = 10000
+print(f"Funds Available: {funds}")
+
+print("\n")
