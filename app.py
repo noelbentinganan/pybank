650e6a0c27fc358f916fa18c4a4ba405a2bf153b
John Noel <jnoelbentinganan@gmail.com>
2025-03-03 23:56:25 +1100

    wip withdrawal fn

--- app.py
+++ app.py
@@ -5,8 +5,26 @@ print("** \t \t \t \t **")
 print(35 * "*")
 print("\n")
 
+
 # Funds
-funds = 10000
-print(f"Funds Available: {funds}")
+available_funds = 10000
+print(f"Funds Available: {available_funds}")
+
+# withdraw fn
+def withdraw(amount):
+  return available_funds - amount
 
 print("\n")
+
+print("What would you like to do? \n [1] - Withdraw \n [2] - Deposit \n")
+choice = int(input(""))
+
+if(choice == 1):
+  amount = float(input("Amount: "))
+  print(withdraw(amount))
+  available_funds = amount
+  print(available_funds)
+elif(choice == 2):
+  print("")
+  
+
